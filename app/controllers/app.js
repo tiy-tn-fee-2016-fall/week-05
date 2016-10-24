c25f0a4666d74e9d0662d8c04b687c88a3edebb2
Ryan Tablada <ryan.tablada@gmail.com>
2016-10-24 11:22:51 -0500

    App controller wiring and setup

---
+++ app/controllers/app.js
@@ -0,0 +1,18 @@
+export default class AppController {
+  // The setup for our new instance
+  /**
+   * appElement: Element : An element wrapping the entire application HTML
+   */
+  constructor(appElement) {
+    this.appElement = appElement;
+    this.model = [];
+  }
+
+  start() {
+    // Grab our data from the API
+    // When the data comes back let's set up some views to show that data
+    // Render the views once they are created
+
+    // Setup a view to handle our form being submitted
+  }
+}
--- app/index.js
+++ app/index.js
@@ -1,1 +1,7 @@
 import 'whatwg-fetch';
+import AppController from './controllers/app';
+
+const el = document.querySelector('.app');
+const application = new AppController(el);
+
+application.start();
