f52dbb58cc484d6fe4585392bb5e1ae620cd6d7b
Ryan Tablada <ryan.tablada@gmail.com>
2016-10-27 10:47:13 -0500

    Hardcoded array data

--- app/index.js
+++ app/index.js
@@ -1,1 +1,19 @@
 import 'whatwg-fetch';
+import Vue from 'vue/dist/vue';
+
+const app = new Vue({
+  el: '.app',
+
+  data() {
+    return {
+      name: 'Hello from Javascript',
+
+      pokemon: [
+        'Pikachu',
+        'Bulbasaur',
+        'Charmander',
+        'Squirtle',
+      ],
+    };
+  },
+});
