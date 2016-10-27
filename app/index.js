22e139e744309bc4327dab4d033acdbbcf16966f
Ryan Tablada <ryan.tablada@gmail.com>
2016-10-27 11:09:00 -0500

    Get Pokemon data from api

--- app/index.js
+++ app/index.js
@@ -5,29 +5,17 @@ const app = new Vue({
   el: '.app',
 
   data() {
-    const model = {
-      name: 'Hello from Javascript',
-
-      pokemon: [
-        'Pikachu',
-        'Bulbasaur',
-        'Charmander',
-        'Squirtle',
-      ],
+    return {
+      name: '',
+      pokemon: null,
     };
+  },
 
-    setTimeout(() => {
-      alert('Charmander is evolving!!!');
-
-      this.pokemon = this.pokemon.map((p) => {
-        if (p === 'Charmander') {
-          return 'Charmeleon';
-        }
-
-        return p;
+  mounted() {
+    fetch('http://pokeapi.co/api/v2/pokemon')
+      .then((r) => r.json())
+      .then((data) => {
+        this.pokemon = data.results;
       });
-    }, 5000);
-
-    return model;
   },
 });
