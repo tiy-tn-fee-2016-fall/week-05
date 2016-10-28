698311dfaec9869cd726cdb0e35a9d4387d0a139
Ryan Tablada <ryan.tablada@gmail.com>
2016-10-28 12:00:47 -0500

    Local toggle for component

--- app/components/stat-bar.js
+++ app/components/stat-bar.js
@@ -3,15 +3,32 @@ import Vue from 'vue/dist/vue';
 export default Vue.extend({
   props: ['info'],
 
+  data() {
+    return {
+      extraPoints: 0,
+    };
+  },
+
+  methods: {
+    increment() {
+      this.extraPoints ++;
+    },
+  },
+
   template: `
   <li class="pokemon-data__stats-item">
     <span class="stats-label">{{ info.stat.name }}</span>
 
     <div class="stats-bar">
       <span class="stats-bar__fill"
-        v-bind:style="{flexBasis: info.base_stat / 150 * 100 + '%'}">
+        v-bind:style="{flexBasis: (info.base_stat + extraPoints) / 150 * 100 + '%'}">
         {{ info.base_stat }}
+        <template v-if="extraPoints">
+          (+{{ extraPoints }})
+        </template>
       </span>
     </div>
+
+    <button className="btn" v-on:click="increment">+</button>
   </li>`,
 });
